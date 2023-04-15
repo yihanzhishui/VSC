6ad54627778d9bcd93afc673f68a0a498e99b423
yihanzhishui <321840565@qq.com>
2023-04-15 23:23:39 +0800

    time

--- push.bat
+++ push.bat
@@ -1,5 +1,8 @@
 @echo off
 rem git push
 git add .
-git commit -m  "from LEGION"
+time_=%date:~0,4%-%date:~5,2%-%date:~8,2%_%time:~0,2%_%time:~3,2%_%time:~6,2%_
+str=from LEGION
+name_=$time str
+git commit -m  time
 git push
